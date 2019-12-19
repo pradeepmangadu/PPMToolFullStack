761a3638d6de9bfbe324af08d9a8f9d37ae4306c
pradeep kumar <spkumar1994@gmail.com>
2019-12-19 17:28:15 +0900

    branch 16 commit

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <Dashboard></Dashboard>
-    </div>
+    <Router>
+      <div className="App">
+        <Header></Header>
+        <Route exact path="/dashboard" component={Dashboard}></Route>
+        <Route exact path="/addProject" component={AddProject}></Route>
+      </div>
+    </Router>
   );
 }
 
