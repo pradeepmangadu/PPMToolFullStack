9d8cd14976e79d1d72534cb6be0982ebf8af774c
pradeep kumar <spkumar1994@gmail.com>
2019-12-23 13:47:44 +0900

    commmit after routing to update screen

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
+import UpdateProject from "./components/project/UpdateProject";
 
 function App() {
   return (
@@ -16,6 +17,11 @@ function App() {
           <Header></Header>
           <Route exact path="/dashboard" component={Dashboard}></Route>
           <Route exact path="/addProject" component={AddProject}></Route>
+          <Route
+            exact
+            path="/updateProject/:id"
+            component={UpdateProject}
+          ></Route>
         </div>
       </Router>
     </Provider>
