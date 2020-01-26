a599baa0a50d672dbe16ed339c0329cb403e3b9e
pradeep kumar <spkumar1994@gmail.com>
2020-01-26 20:27:00 +0900

    Organize Project Tasks by status and priority

--- ppmtool-react-client/src/components/ProjectBoard/Backlog.js
+++ ppmtool-react-client/src/components/ProjectBoard/Backlog.js
@@ -7,6 +7,23 @@ class Backlog extends Component {
     const tasks = project_tasks_prop.map(project_task => (
       <ProjectTask key={project_task.id} project_task={project_task} />
     ));
+
+    let todoItems = [];
+    let inprogressItems = [];
+    let doneItems = [];
+
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].props.project_task.status === "TO_DO") {
+        todoItems.push(tasks[i]);
+      }
+      if (tasks[i].props.project_task.status === "IN_PROGRESS") {
+        inprogressItems.push(tasks[i]);
+      }
+      if (tasks[i].props.project_task.status === "DONE") {
+        doneItems.push(tasks[i]);
+      }
+    }
+
     return (
       <div className="container">
         <div className="row">
@@ -16,7 +33,7 @@ class Backlog extends Component {
                 <h3>TO DO</h3>
               </div>
             </div>
-            {tasks}
+            {todoItems}
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
@@ -24,10 +41,7 @@ class Backlog extends Component {
                 <h3>In Progress</h3>
               </div>
             </div>
-            {
-              // <!-- SAMPLE PROJECT TASK STARTS HERE -->
-              // <!-- SAMPLE PROJECT TASK ENDS HERE -->
-            }
+            {inprogressItems}
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
@@ -35,10 +49,7 @@ class Backlog extends Component {
                 <h3>Done</h3>
               </div>
             </div>
-            {
-              // <!-- SAMPLE PROJECT TASK STARTS HERE -->
-              //  <!-- SAMPLE PROJECT TASK ENDS HERE -->
-            }
+            {doneItems}
           </div>
         </div>
       </div>
