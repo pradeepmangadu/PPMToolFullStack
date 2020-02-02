5bc8e6e55da805da997bddb23147c3dcb67ab26a
pradeep kumar <spkumar1994@gmail.com>
2020-02-02 18:41:51 +0900

    User Registration Part-3

---
+++ PPMToolFullStack/src/main/java/com/hr/ppmtool/web/UserController.java
@@ -0,0 +1,47 @@
+package com.hr.ppmtool.web;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hr.ppmtool.Validator.UserValidator;
+import com.hr.ppmtool.domain.User;
+import com.hr.ppmtool.services.MapValidationErrorService;
+import com.hr.ppmtool.services.UserService;
+
+@RestController
+@RequestMapping("/api/users/")
+public class UserController {
+
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private UserValidator userValidator;
+
+	@PostMapping("/register")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
+
+		// Validate Password Match
+		userValidator.validate(user, result);
+
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+		if (errorMap != null)
+			return errorMap;
+
+		User newUser = userService.saveUser(user);
+		return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+
+	}
+
+}
