c6c4fc8470b0457d322aa3eb0820c1af0b39250f
pradeep kumar <spkumar1994@gmail.com>
2020-02-02 16:38:26 +0900

    Create User Object, Validation, Repository, Service

--- PPMToolFullStack/src/main/java/com/hr/ppmtool/domain/User.java
+++ PPMToolFullStack/src/main/java/com/hr/ppmtool/domain/User.java
@@ -1,5 +1,110 @@
 package com.hr.ppmtool.domain;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Email(message = "Username needs to be an email")
+	@NotBlank(message = "username is required")
+	@Column(unique = true)
+	private String username;
+
+	@NotBlank(message = "Please enter your full name")
+	private String fullname;
+	@NotBlank(message = "Password field is required")
+	private String password;
+
+	@Transient
+	private String confirmPassword;
+
+	private Date created_at;
+
+	private Date updated_at;
+
+	public User() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public Date getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+
+	public Date getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(Date updated_at) {
+		this.updated_at = updated_at;
+	}
+
+	@PrePersist
+	protected void onCreate() {
+		this.created_at = new Date();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		this.updated_at = new Date();
+	}
 
 }
