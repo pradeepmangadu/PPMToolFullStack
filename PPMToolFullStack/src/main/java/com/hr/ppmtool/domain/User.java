3cac9df91c0547a3c2af23622653520191578b12
pradeep kumar <spkumar1994@gmail.com>
2020-02-02 16:20:30 +0900

    Initial Security Config

---
+++ PPMToolFullStack/src/main/java/com/hr/ppmtool/domain/User.java
@@ -0,0 +1,5 @@
+package com.hr.ppmtool.domain;
+
+public class User {
+
+}
---
+++ PPMToolFullStack/src/main/java/com/hr/ppmtool/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.hr.ppmtool.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+
+	private JWTAuthenticationEntryPoint unauthorizedHandler;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(auth);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().headers()
+				.frameOptions().sameOrigin() // To
+												// enable
+												// h2-console
+				.and().authorizeRequests().antMatchers("/", "/favicon.ico", "/**/*.png", "/**/*.jpg", "/**/*.gif",
+						"/**/*.svg", "/**/*.html", "/**/*.js", "/**/*.css")
+				.permitAll().anyRequest().authenticated();
+	}
+
+}
