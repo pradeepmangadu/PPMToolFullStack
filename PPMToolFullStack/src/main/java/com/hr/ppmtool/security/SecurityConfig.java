a81252ed1887b1f27424995dfa86a0c85eda4195
pradeep kumar <spkumar1994@gmail.com>
2020-02-03 20:07:45 +0900

    Custom JWT filter to use our tokens

--- PPMToolFullStack/src/main/java/com/hr/ppmtool/security/SecurityConfig.java
+++ PPMToolFullStack/src/main/java/com/hr/ppmtool/security/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.hr.ppmtool.services.CustomUserDetailsService;
 
@@ -28,6 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
+
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter();
+	}
 
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -55,6 +61,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 						"/**/*.js", "/**/*.css")
 				.permitAll().antMatchers(SIGN_UP_URLS).permitAll().antMatchers(H2_URL).permitAll().anyRequest()
 				.authenticated();
+
+		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 }
