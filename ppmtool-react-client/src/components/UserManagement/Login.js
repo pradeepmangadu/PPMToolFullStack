76eddbc268a4702aec947fe6da71928ad907791a
pradeep kumar <spkumar1994@gmail.com>
2020-02-09 18:30:07 +0900

    Lock public routes when logged in

--- ppmtool-react-client/src/components/UserManagement/Login.js
+++ ppmtool-react-client/src/components/UserManagement/Login.js
@@ -17,6 +17,13 @@ class Login extends Component {
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
+
+  componentDidMount() {
+    if (this.props.security.validToken) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.security.validToken) {
       this.props.history.push("/dashboard");
